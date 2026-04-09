c2f5cb928209ba7b7baaf602f76cb70372a11f70
Minh Do <155079466+hypeaxis@users.noreply.github.com>
2026-04-09 10:02:18 +0700

    Fix next.js versionfor vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -184,70 +184,74 @@ export default function Home() {
 
                     </div>
                 </section>
-
-                {/* 4. Smart Liquidity, Simple Steps */}
-                <section className="mx-auto flex max-w-[1440px] flex-col items-center justify-center px-4 py-[32px] lg:h-[800px] lg:px-[119px]"
+            </main>
+{/* 4. Smart Liquidity, Simple Steps */}
+                <section 
+                    className="w-full"
                     style={{
                         background: "linear-gradient(180deg, #121817 -24.62%, #192422 137.47%)"
                     }}
                 >
-                    <h2 className='mb-[81px] text-center'>Smart Liquidity, Simple Steps</h2>
-                    <div className='grid w-full grid-cols-1 gap-[24px] md:grid-cols-3 lg:h-[292px]'>
-                        <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
-                            <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
-                                <div 
-                                    className="text-[64px] font-[800] leading-none"
-                                    style={{
-                                        background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
-                                        WebkitBackgroundClip: "text",
-                                        WebkitTextFillColor: "transparent"
-                                    }}
-                                >1
+                    <div className="mx-auto flex max-w-[1440px] flex-col items-center justify-center px-4 py-[32px] lg:h-[800px] lg:px-[119px]">
+                        <h2 className='mb-[81px] text-center'>Smart Liquidity, Simple Steps</h2>
+                        <div className='grid w-full grid-cols-1 gap-[24px] md:grid-cols-3 lg:h-[292px]'>
+                            
+                            <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
+                                <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
+                                    <div 
+                                        className="text-[64px] font-[800] leading-none"
+                                        style={{
+                                            background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
+                                            WebkitBackgroundClip: "text",
+                                            WebkitTextFillColor: "transparent"
+                                        }}
+                                    >1
+                                    </div>
+                                    <h3 className='text-[24px]'>Create Position & Deposit</h3>
+                                    <p className='text-[16px]'>
+                                        Add liquidity and start earning immediately with no complex setup.
+                                    </p>
                                 </div>
-                                <h3 className='text-[24px]'>Create Position & Deposit</h3>
-                                <p className='text-[16px]'>
-                                    Add liquidity and start earning immediately with no complex setup.
-                                </p>
                             </div>
-                        </div>
-                        <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
-                            <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
-                                <div 
-                                    className="text-[64px] font-[800] leading-none"
-                                    style={{
-                                        background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
-                                        WebkitBackgroundClip: "text",
-                                        WebkitTextFillColor: "transparent"
-                                    }}
-                                >2
+                            
+                            <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
+                                <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
+                                    <div 
+                                        className="text-[64px] font-[800] leading-none"
+                                        style={{
+                                            background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
+                                            WebkitBackgroundClip: "text",
+                                            WebkitTextFillColor: "transparent"
+                                        }}
+                                    >2
+                                    </div>
+                                    <h3 className='text-[24px]'>Auto - manage</h3>
+                                    <p className='text-[16px]'>
+                                        Luxor automatically places, adjusts, and rebalances your liquidity for optimal ranges.
+                                    </p>
                                 </div>
-                                <h3 className='text-[24px]'>Auto - manage</h3>
-                                <p className='text-[16px]'>
-                                    Luxor automatically places, adjusts, and rebalances your liquidity for optimal ranges.
-                                </p>
                             </div>
-                        </div>
-                        <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
-                            <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
-                                <div 
-                                    className="text-[64px] font-[800] leading-none"
-                                    style={{
-                                        background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
-                                        WebkitBackgroundClip: "text",
-                                        WebkitTextFillColor: "transparent"
-                                    }}
-                                >3
+                            
+                            <div className="group flex flex-col items-start justify-center rounded-[8px] border border-[#87F5CE]/30 bg-[#1C2726] p-[36px] w-[384px] h-[292px]">
+                                <div className="flex flex-col items-start w-[312px] h-[173px] gap-[24px]">
+                                    <div 
+                                        className="text-[64px] font-[800] leading-none"
+                                        style={{
+                                            background: "linear-gradient(180deg, #8CF8D2 25.38%, rgba(65, 189, 199, 0) 79.31%)",
+                                            WebkitBackgroundClip: "text",
+                                            WebkitTextFillColor: "transparent"
+                                        }}
+                                    >3
+                                    </div>
+                                    <h3 className='text-[24px]'>Earn Fee</h3>
+                                    <p className='text-[16px]'>
+                                        Earn higher, more stable returns through automated strategies.
+                                    </p>
                                 </div>
-                                <h3 className='text-[24px]'>Earn Fee</h3>
-                                <p className='text-[16px]'>
-                                    Earn higher, more stable returns through automated strategies.
-                                </p>
                             </div>
                         </div>
                     </div>
                 </section>
-            </main>
-
             {/* Footer */}
             <footer className="flex w-full flex-col items-center"
                     style={{ 
